f423fddbd0d852a84b58fbf2ace77edaabd11a45
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2022-01-21 19:37:02 +0200

    Show only the result specific value when querying detail values for a result

--- app/types/decidim/accountability_simple/result_detail_type.rb
+++ app/types/decidim/accountability_simple/result_detail_type.rb
@@ -13,7 +13,18 @@ module Decidim
       field :position, !types.Int, "The position of this detail"
       field :title, !Decidim::Core::TranslatedFieldType, "The title of this detail."
       field :icon, types.String, "The icon handle of this detail"
-      field :values, !types[Decidim::AccountabilitySimple::ResultDetailValueType]
+      field :values, !types[Decidim::AccountabilitySimple::ResultDetailValueType], "The current installation's name." do
+        resolve ->(obj, _args, ctx) {
+          parent_ctx = ctx.parent&.parent
+          if parent_ctx && parent_ctx.object.is_a?(Decidim::Accountability::Result)
+            result = parent_ctx.object
+            return [obj.value_for(result)]
+          end
+
+          # Default e.g. for component context, return all values
+          obj.values
+        }
+      end
     end
 
     # To update to the new GraphQL API
@@ -25,6 +36,18 @@ module Decidim
     #   field :position, GraphQL::Types::Int, description: "The position of this detail", null: false
     #   field :title, Decidim::Core::TranslatedFieldType, description: "The title of this detail.", null: false
     #   field :icon, GraphQL::Types::String, description: "The icon handle of this detail", null: true
+    #   field :values, Decidim::AccountabilitySimple::ResultDetailValueType, null: false
+
+    #   def values
+    #     parent_ctx = context.parent&.parent
+    #     if parent_ctx && parent_ctx.object.is_a?(Decidim::Accountability::Result)
+    #       result = parent_ctx.object
+    #       return [object.value_for(result)]
+    #     end
+    #
+    #     # Default e.g. for component context, return all values
+    #     object.values
+    #   end
     # end
   end
 end
