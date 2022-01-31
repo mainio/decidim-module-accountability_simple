b5117db7549d89b5f9b9c1895d514b75e173c383
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2022-01-31 21:55:24 +0200

    Increase the allowed upload size

--- app/uploaders/decidim/accountability_simple/list_image_uploader.rb
+++ app/uploaders/decidim/accountability_simple/list_image_uploader.rb
@@ -4,6 +4,10 @@ module Decidim
   module AccountabilitySimple
     class ListImageUploader < ::Decidim::ImageUploader
       process resize_to_limit: [850, 320]
+
+      def max_image_height_or_width
+        8000
+      end
     end
   end
 end
